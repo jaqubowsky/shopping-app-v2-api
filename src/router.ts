9e77dbcc5c67dac5972272e82672cfb70eedf27f
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-03 23:17:36 +0200

    Add sample message to get route

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,12 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/product", () => {});
+router.get("/product", (req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
 router.get("/product/:id", () => {});
 router.put("/product/:id", () => {});
 router.post("/product", () => {});
 router.delete("/product/:id", () => {});
+
+export default router
