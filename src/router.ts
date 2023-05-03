4837410faca8fdc43a8026826eed406b55bae9f4
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-03 22:03:23 +0200

    Create sample routes

---
+++ src/router.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/product", () => {});
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.post("/product", () => {});
+router.delete("/product/:id", () => {});
