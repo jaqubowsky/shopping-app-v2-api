42f9641ad54c2836e68eac181acb81a39528d069
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-03 23:32:53 +0200

    Sample error handling

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -2,32 +2,42 @@ import prisma from "../db";
 import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
 
 export const createNewUser = async (req, res) => {
-  const user = await prisma.user.create({
-    data: {
-      username: req.body.username,
-      email: req.body.email,
-      password: await hashPassword(req.body.password),
-    },
-  });
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        email: req.body.email,
+        password: await hashPassword(req.body.password),
+      },
+    });
 
-  const token = createJWT(user);
-  res.json({ token });
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Authentication error" });
+  }
 };
 
 export const signIn = async (req, res) => {
-  const user = await prisma.user.findUnique({
-    where: {
-      username: req.body.username,
-      email: req.body.email,
-    },
-  });
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: req.body.username,
+        email: req.body.email,
+      },
+    });
 
-  const isValid = await comparePasswords(req.body.password, user.password);
+    const isValid = await comparePasswords(req.body.password, user.password);
 
-  if (!isValid) {
-    return res.status(401).json({ message: "Invalid password" });
+    if (!isValid) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (error) {
+    res.status(400).json({ message: "Invalid username or email" });
   }
-
-  const token = createJWT(user);
-  res.json({ token });
 };
