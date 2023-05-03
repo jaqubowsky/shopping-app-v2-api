c8a8f1242616406617ea7407e60842530fa65b4b
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-03 23:17:14 +0200

    Add middleware and auth routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import morgan from "morgan";
+import router from "./router";
+import cors from "cors";
+import { protect } from "./modules/auth";
+import { createNewUser, signIn } from "./handlers/user";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello World!" });
-});
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", protect, router);
+
+app.post("/register", createNewUser);
+app.post("/login", signIn);
 
 export default app;
