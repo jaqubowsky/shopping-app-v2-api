b1c3797d4cb3b6f6a868e95bc443f261bd097a55
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-04 21:26:35 +0200

    Add env port variables

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import * as dotenv from "dotenv";
 dotenv.config();
+import config from "./config";
 
 import app from "./server";
-const port = 5000;
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+app.listen(config.port, () => {
+  console.log(`Server running on http://localhost:${config.port}/`);
 });
