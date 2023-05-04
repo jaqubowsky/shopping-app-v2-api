5c4b5d681842a582860ecc43096665ad9c7bc059
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-04 21:13:17 +0200

    Sample error handling

--- src/server.ts
+++ src/server.ts
@@ -17,4 +17,14 @@ app.use("/api", protect, router);
 app.post("/register", createNewUser);
 app.post("/login", signIn);
 
+app.use((err, req, res, next) => {
+  if (err.type === "auth") {
+    return res.status(401).json({ error: "Unauthorized" });
+  } else if (err.type === "input") {
+    return res.status(400).json({ error: "Invalid input!" });
+  } else {
+    return res.status(err.code || 500).json({ error: err.message });
+  }
+});
+
 export default app;
