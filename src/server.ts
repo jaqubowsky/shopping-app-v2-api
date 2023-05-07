dec56d96dd23917ee1c658237a5b0b068c2eb058
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-07 12:46:49 +0200

    Add cookies and logout function

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ import morgan from "morgan";
 import router from "./router";
 import cors from "cors";
 import { protect } from "./modules/auth";
-import { createNewUser, signIn } from "./handlers/user";
+import cookieParser from "cookie-parser";
+import { createNewUser, logout, signIn } from "./handlers/user";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
@@ -16,6 +18,7 @@ app.use("/api", protect, router);
 
 app.post("/register", createNewUser);
 app.post("/login", signIn);
+app.get("/logout", logout);
 
 app.use((err, req, res, next) => {
   if (err.type === "auth") {
