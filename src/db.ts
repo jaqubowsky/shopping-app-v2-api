b47bd126c57d8ba18e190a1a027ca91d7a680c34
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-09 22:50:19 +0200

    Add google cloud db

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { Storage } from "@google-cloud/storage";
+import path from "path";
 
 const prisma = new PrismaClient();
+const storage = new Storage({
+  projectId: process.env.GCLOUD_PROJECT_ID,
+  keyFilename: path.join(
+    __dirname,
+    `../${process.env.GCLOUD_APPLICATION_CREDENTIALS}`
+  ),
+});
+
+export const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
 
 export default prisma;
