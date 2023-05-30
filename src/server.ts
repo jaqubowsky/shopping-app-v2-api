d89ed352346b8b65e8e1ca670ae0cb8bcfbd6ad6
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-05-30 11:36:20 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import morgan from "morgan";
 import router from "./router";
+import helmet from "helmet";
 import cors from "cors";
 import { protect } from "./modules/auth";
 import cookieParser from "cookie-parser";
-import { createNewUser, getUserById, loggedIn, logout, signIn } from "./handlers/user";
-import { getAllProducts, getProduct, getUserProducts } from "./handlers/product";
+import {
+  createNewUser,
+  getUserById,
+  loggedIn,
+  logout,
+  signIn,
+} from "./handlers/user";
+import {
+  getAllProducts,
+  getProduct,
+} from "./handlers/product";
 
 const app = express();
 
@@ -18,6 +28,8 @@ app.use(
 );
 app.use(morgan("dev"));
 app.use(express.json());
+app.disable("x-powered-by");
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", protect, router);
@@ -32,8 +44,7 @@ app.get("/user/:id", getUserById);
 app.get("/product/:id", getProduct);
 
 app.use((err, req, res, next) => {
-  // return res.status(err.code || 500).json({ error: err.message });
-  return res.status(err.code).json({ error: err.message });
+  return res.status(err.code).json({ error: err });
 });
 
 export default app;
