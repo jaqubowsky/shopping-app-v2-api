af5fbac8932a43cd0e67ca5e2aecb1cfc519c6f3
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-09 23:57:22 +0200

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as dotenv from "dotenv";
-dotenv.config();
 import config from "./config";
 
 import app from "./server";
+
+dotenv.config();
 
 app.listen(config.port, () => {
   console.log(`Server running on http://localhost:${config.port}/`);
