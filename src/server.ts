5f24e142f9679e597d4779e9e5b596dd956d4892
Jakub Nalewajk <jaqubowskyy@gmail.com>
2023-06-10 01:15:57 +0200

    Fix

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://boisterous-pastelito-fa5201.netlify.app/",
     credentials: true,
   })
 );
